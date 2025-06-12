15bb249d651a7ec4dad988105c094d8bc19c0da1
Jellytooooooo <taegu234@naver.com>
2025-06-13 00:11:15 +0900

    Initial commit

---
+++ Source/BossDesign/BossDesignGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BossDesignGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABossDesignGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABossDesignGameMode();
+};
+
+
+
